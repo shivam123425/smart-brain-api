cd56a7a9da004d2313ff303c18ff43b3f21c2c75
shivam123425 <shivam123425@gmail.com>
2018-11-13 18:59:19 +0530

    Add seed data to the database

--- postgres/deploy_schemas.sql
+++ postgres/deploy_schemas.sql
@@ -1,3 +1,7 @@
 -- Deploy fresh database tables
 \i '/docker-entrypoint-initdb.d/tables/login.sql'
 \i '/docker-entrypoint-initdb.d/tables/users.sql'
+
+-- Seed the database
+\i '/docker-entrypoint-initdb.d/seed/seed.sql'
+
---
+++ postgres/seed/seed.sql
@@ -0,0 +1,13 @@
+BEGIN TRANSACTION;
+
+INSERT INTO login
+    (hash,email)
+VALUES
+    ('$2a$10$O9/QpK4krD6y7hlWEy3rp.RSS1py15D/lBc7UW6BpdvaBRAwgfrhi', 'shivam@example.com');
+
+INSERT INTO users
+    (name,email,entries, joined)
+VALUES
+    ('Shivam', 'shivam@example.com', 5, '2018-01-01');
+
+COMMIT;
